3bcc5a9aa85b88cf15970e22a0e0116e94208061
Majoha <Majoha>
2019-01-17 23:15:20 +0000

    will move sprite

--- hellloSFML/hellloSFML.cpp
+++ hellloSFML/hellloSFML.cpp
@@ -9,6 +9,9 @@ using namespace sf;
 
 int main()
 {
+
+	int i = 0;
+
 	//This is a test
 	VideoMode vm(1280, 800);
 	RenderWindow window(vm, "Hello, world", Style::Fullscreen);
@@ -17,13 +20,18 @@ int main()
 	textureBackground.loadFromFile("graphics/background.png");
 	Sprite spriteBackground;
 	spriteBackground.setTexture(textureBackground);
-	spriteBackground.setPosition(0, 0);
+	
 	while (window.isOpen())
 	{
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
+
+
+		spriteBackground.setPosition(i++, 0);
+
+
 
 		window.clear();
 		window.draw(spriteBackground);
